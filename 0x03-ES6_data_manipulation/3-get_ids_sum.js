fbef372182a19edc2d6793b64b4f4a0352239a41
Morabet <ismailelmorabet2@gmail.com>
2024-04-15 22:31:18 +0000

    fix 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,14 +1,13 @@
-import getListStudentIds from "./1-get_list_student_ids";
+import getListStudentIds from './1-get_list_student_ids';
 
 /**
  * a function that returns the sum of all the student ids.
  * @param {array} listStudents - array of object students
  * @return {number} - sum of all ids
  */
-
 export default function getStudentIdsSum(listStudents) {
   const ids = getListStudentIds(listStudents);
 
-  const sum = ids.reduce((acc, current) => acc + current);
+  const sum = ids.reduce((ac, id) => ac + id, 0);
   return sum;
 }
