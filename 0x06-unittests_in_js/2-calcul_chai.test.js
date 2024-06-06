d64f1e89534e2f8e8d8902bcbe430429dbe57442
Morabet <ismailelmorabet2@gmail.com>
2024-06-06 21:40:03 +0000

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,17 +2,22 @@ const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', () => {
-  it('should return 6', () => {
-    expect(calculateNumber('SUM', 1.4, 4.6)).to.equal(6);
+  describe('type == "SUM"', () => {
+    it('should return 6', () => {
+      expect(calculateNumber('SUM', 1.4, 4.6)).to.equal(6);
+    });
   });
-  it('should return 0', () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 1.4)).to.equal(0);
+  describe('type == "SUBTRACT"', () => {
+    it('should return 0', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 1.4)).to.equal(0);
+    });
   });
-  it('should return Error', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-  });
-  it('should return 1', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 1.4)).to.equal(1);
+  describe('type == "DIVIDE"', () => {
+    it('should return Error', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+    it('should return 1', () => {
+      expect(calculateNumber('DIVIDE', 1.4, 1.4)).to.equal(1);
+    });
   });
 });
-
