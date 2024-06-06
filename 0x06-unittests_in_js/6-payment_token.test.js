788765d7018ecf5949045f548cce94e1ce2de19d
Morabet <ismailelmorabet2@gmail.com>
2024-06-06 21:31:52 +0000

    task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,12 +4,8 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromAPI', () => {
   it('getPaymentTokenFromAPI(success), where success == true', () => new Promise((done) => {
     getPaymentTokenFromAPI(true).then((res) => {
-      try {
-        expect(res).to.deep.equal({ data: 'Successful response from the API' });
-        done();
-      } catch (error) {
-        done(error);
-      }
+      expect(res).to.deep.equal({ data: 'Successful response from the API' });
+      done();
     });
   }));
 });
